ebd1ebcc19a25837ec78b898f88045e8367a6fbe
Anders <anderskoch.airbag@gmail.com>
2017-12-10 14:31:01 +0100

    debug comments gone

--- day4/high_entropy_passphrase.js
+++ day4/high_entropy_passphrase.js
@@ -4,12 +4,10 @@ file.getInputData('input.txt')
 	.then((data) => {
 		const input = data.toString().split('\r\n');
 		const invalid1 = countRepeatedWords(input);
-		console.log(invalid1);
 		const valid1 = input.length - invalid1;
 		console.log('first half (repeated words) ', valid1, ' valid passphrases');
 		//isnert suhioteshiop
 		const invalid2 = countRepeatedWords(input, isAnagram);
-		console.log(invalid2);
 		const valid2 = input.length - invalid2;
 		console.log('second half (anagram words) ', valid2, ' valid passphrases');
 	})
@@ -22,7 +20,6 @@ function isRepeatedWord(str, comparisonFunc) {
 	for (let i = 0; i < arr.length; ++i) {
 		for (let j = 0; j < arr.length; ++j) {
 			if (i !== j && comparisonFunc(arr[i], arr[j])) {
-				console.log('arr[i] ', arr[i], ' ', ' arr[j] ', arr[j]);
 				return true;
 			}
 		}
