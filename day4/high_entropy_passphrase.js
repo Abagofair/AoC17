67aaa000686095e7aa7b6cba580b6c3be813dafe
Anders <anderskoch.airbag@gmail.com>
2017-12-10 14:30:27 +0100

    fourth day both halfs solved

--- day4/high_entropy_passphrase.js
+++ day4/high_entropy_passphrase.js
@@ -3,19 +3,26 @@ const file = require('../utility.js');
 file.getInputData('input.txt')
 	.then((data) => {
 		const input = data.toString().split('\r\n');
-		const invalid = countRepeatedWords(input);
-		const valid = input.length - invalid;
-		console.log(valid);
+		const invalid1 = countRepeatedWords(input);
+		console.log(invalid1);
+		const valid1 = input.length - invalid1;
+		console.log('first half (repeated words) ', valid1, ' valid passphrases');
+		//isnert suhioteshiop
+		const invalid2 = countRepeatedWords(input, isAnagram);
+		console.log(invalid2);
+		const valid2 = input.length - invalid2;
+		console.log('second half (anagram words) ', valid2, ' valid passphrases');
 	})
 	.catch((err) => {
 		console.log(err);
 	});
 	
-function checkIfRepeatedWord(str) {
+function isRepeatedWord(str, comparisonFunc) {
 	let arr = str.split(' ');
 	for (let i = 0; i < arr.length; ++i) {
 		for (let j = 0; j < arr.length; ++j) {
-			if (i !== j && arr[i] === arr[j]) {
+			if (i !== j && comparisonFunc(arr[i], arr[j])) {
+				console.log('arr[i] ', arr[i], ' ', ' arr[j] ', arr[j]);
 				return true;
 			}
 		}
@@ -23,10 +30,28 @@ function checkIfRepeatedWord(str) {
 	return false;
 }
 
-function countRepeatedWords(strArr) {
+function isAnagram(w1, w2) {
+	const w1_arr = w1.split('');
+	const w2_arr = w2.split('');
+	const lengths = {w1: w1_arr.length, w2: w2_arr.length};
 	let count = 0;
+	for (let i = 0; i < w1_arr.length; ++i) {
+		for (let j = 0; j < w2_arr.length; ++j) {
+			if (w1_arr[i] === w2_arr[j]) {
+				++count;
+				w2_arr.splice(j, 1);
+			}
+		}
+	}
+	return (count === lengths.w1 && count === lengths.w2);
+}
+
+function countRepeatedWords(strArr, comparisonFunc) {
+	let count = 0;
+	if (comparisonFunc === undefined)
+		comparisonFunc = function(w1,w2) {return w1 === w2};
 	for (let i = 0; i < strArr.length; ++i) {
-		if (checkIfRepeatedWord(strArr[i]))
+		if (isRepeatedWord(strArr[i], comparisonFunc))
 			++count;
 	}
 	return count;
